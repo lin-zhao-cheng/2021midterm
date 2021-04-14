80870cbc3ee948fb6b87711be60b0ce92ff0a62d
lin-zhao-cheng <jjaacckk41208@gmail.com>
2021-04-15 01:47:18 +0800

    init

---
+++ src/components/Header.js
@@ -0,0 +1,10 @@
+import NavBar from "./NavBar";
+// import { Link } from "react-router-dom"
+
+export default function Header({title}) {
+   return (
+      <div className="header">
+         <NavBar />
+      </div>
+   );
+}
